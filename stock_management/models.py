af00f01c0b670400b8944b82973dc9881c7646ea
Olusegun <segestic@gmail.com>
2021-01-04 11:06:58 +0200

    edit

--- stock_management/models.py
+++ stock_management/models.py
@@ -10,21 +10,13 @@ category_choice = (
 	('Phone', 'Phone'),
 )
 
-# approval_choice = (
-# 	('Approved', 'Approved'),
-# 	('Rejected', 'Rejected'),
-# )
 
 class Category(models.Model):
 	name = models.CharField(max_length=50, blank=True, null=True)
-	# models.CharField(choices=category_choice,  default='D')
+
 
 	def __str__(self):
 		return self.name
-
-# class Approval(models.Model):
-# 	name = models.BooleanField(default=False)
-	# name = models.CharField(max_length=50, default=True, blank=True, null=True)
 
 class Stock(models.Model):
 	category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True)
@@ -42,19 +34,12 @@ class Stock(models.Model):
 	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 	export_to_CSV = models.BooleanField(default=False)
 	approval = models.CharField(max_length=10, blank=True, null=True)
-	# auto_now add is first_add_time while auto_now is lastupdated time
-	# approval = models.BooleanField(default=False)
-	# approval = models.ForeignKey(Approval, on_delete=models.CASCADE)
-	# stockrequesthistory = models.ForeignKey(StockRequestHistory, on_delete=models.CASCADE)
-	# approval = models.BooleanField(default=False)
-	# request_history = models.ForeignKey(StockRequestHistory, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.item_name
 
 class StockHistory(models.Model):
 	category = models.CharField(max_length=50, blank=True, null=True)
-	# id = models.ForeignKey(Stock, on_delete=models.CASCADE)
 	item_name = models.CharField(max_length=50, blank=True, null=True)
 	quantity = models.IntegerField(default='0', blank=True, null=True)
 	receive_quantity = models.IntegerField(default='0', blank=True, null=True)
@@ -85,8 +70,5 @@ class StockRequestHistory(models.Model):
 	# approval = models.BooleanField(default=False)
 	approval = models.CharField(max_length=10, blank=False, null=False)
 
-	#
-	# def __str__(self):
-	# 	return self.name
 
 
