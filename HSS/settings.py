6d7d2e94272abb987a1eb0add35f7353119ed38b
Olusegun <segestic@gmail.com>
2021-02-06 18:30:22 +0200

    edit

--- HSS/settings.py
+++ HSS/settings.py
@@ -88,7 +88,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        'NAME': os.path.join(BASE_DIR, 'db1.sqlite3')
     }
 }
 
