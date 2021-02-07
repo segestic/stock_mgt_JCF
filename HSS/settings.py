28b86b853b33c78091bd1ff317bb3af9e1a9fe19
Olusegun <segestic@gmail.com>
2021-02-07 18:46:35 +0200

    impt update

--- HSS/settings.py
+++ HSS/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '33^&so#2*m1=*80vu^ze*p1ya%ckd)m5%u+h^x@sud@z6_!y2a'
+SECRET_KEY = '123456'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
